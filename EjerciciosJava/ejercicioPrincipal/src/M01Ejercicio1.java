1c023aaa8f9a8f72d58fce7ae16e2c83a8d7f31f
huamanxd <hchristian6@live.com>
2026-07-18 03:40:12 -0500

    Resolviendo el ejercicio 1

--- EjerciciosJava/ejercicioPrincipal/src/M01Ejercicio1.java
+++ EjerciciosJava/ejercicioPrincipal/src/M01Ejercicio1.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 class M01Ejercicio1 {
     public static void main(String[] args){
-        System.out.println("Holaadx");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre:");
+        String nom = sc.nextLine();
+
+        System.out.println("Cual es su edad: ");
+        byte edad = sc.nextByte();
+
+        System.out.println("cual es su nota final? ");
+        float nota = sc.nextFloat();
+
+        System.out.println("Le gusta programar? (true/ false)");
+        boolean programa = sc.nextBoolean();
+
+        System.out.printf("Hola "+nom+" tienes "+edad+" años y tu" +
+                " nota final es "+nota+" tambien se sabe que " +
+                "programar "+programa+" te gusta!");
 
     }
 }
